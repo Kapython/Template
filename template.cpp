db6bf715722aa35b2feccab8971f3a94d5468555
Ka_python <Maxim.Dziuba@gmail.com>
2014-11-20 23:45:50 +0200

    Home CPP

--- template.cpp
+++ template.cpp
@@ -1,1 +1,7 @@
 #include "template.h"
+
+template <class T> Value(){
+    head = NULL;
+    curr = NULL;
+    length = 0;
+};
--- template.h
+++ template.h
@@ -1,12 +1,28 @@
 #include <iostream>
 
-template <class T>
-    class Value{
-    public:
-        Value(int size = 10);
-        Value(const Value &rhs);
+template <class T> class Value{
+    struct Node {
+        T data;
+        Node *prew;
+        Node *next;
+    };
+public:
+    int length;
+    Value();
+    Value(T element);
+    ~value();
+    void initial();
+    void push(T data);
+    T pop(); //Получить элемент и удалить его из списка
+    T get(); //Получить элемент неудаляя его
+    void delData(T data);
+    void printValue();
+    void clear();
+    void isEmpty();
+    void goNext();
+    void goPrew();
 
-    private:
-        T *pValue;
-        int size;
+private:
+    Node *head;
+    Node *curr;
 };
